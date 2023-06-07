18e7f38c0d8114fc42e5f1b8de54aff3f8b0795c
ENDPrasad <e.n.d.prasad@gmail.com>
2023-06-07 17:48:59 +0530

    Add page design

--- src/App.js
+++ src/App.js
@@ -4,20 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Similarity Checker</h1>
+      <div className='form' type='form'>
+        <label>Question 1</label>
+        <input type="text"></input>
+        <label>Question 2</label>
+        <input type="text"></input>
+        <button>Check for similarity</button>        
+      </div>
     </div>
   );
 }
